65e124862a1eaf35df31f9603170cd6edb782a75
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-03-04 20:54:10 +0900

    no changes

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,17 @@
 const makeMenuTab = () => {
   const tabs = document.createElement("div");
+  tabs.classList.add('tab');
   const input = document.createElement("input");
   input.type = "radio";
   input.name = "tabs";
   input.id = "menu-tab";
   tabs.appendChild(input);
   const label = document.createElement("label");
+  label.innerHTML = "Menu";
   label.htmlFor = "menu-tab";
   tabs.appendChild(label);
   const div = document.createElement("div");
   tabs.appendChild(div);
-  const h1 = document.createElement("h1");
-  h1.innerHTML = "Menu";
-  div.appendChild(h1);
   return tabs;
 };
 
