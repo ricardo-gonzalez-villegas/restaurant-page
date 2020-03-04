a2cca01c6a80d2eaf77982870cab84b7c660304f
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-03-04 20:51:00 +0900

    finished home page

--- src/js/home.js
+++ src/js/home.js
@@ -1,27 +1,27 @@
 const makeHomeTab = () => {
-    const tabs = document.createElement("div");
-    const input = document.createElement("input");
-    input.type = "radio";
-    input.name = "tabs";
-    input.id = "home-tab";
-    input.checked = "checked";
-    tabs.appendChild(input);
-    const label = document.createElement("label");
-    label.htmlFor = "home-tab";
-    tabs.appendChild(label);
-    const div = document.createElement("div");
-    tabs.appendChild(div);
-    const h1 = document.createElement("h1");
-    h1.innerHTML = "Home";
-    div.appendChild(h1);
-    return tabs;
-  };
-  
-  const populateHome = () => {
-    const p = document.createElement("p");
-    p.innerHTML = "This is the home tab";
-    return p;
-  };
-  
-  export { makeHomeTab, populateHome };
-  
+  const tabs = document.createElement("div");
+  tabs.classList.add('tab');
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.name = "tabs";
+  input.id = "home-tab";
+  input.checked = "checked";
+  tabs.appendChild(input);
+  const label = document.createElement("label");
+  label.innerHTML = "Home";
+  label.htmlFor = "home-tab";
+  tabs.appendChild(label);
+  const div = document.createElement("div");
+  tabs.appendChild(div);
+  return tabs;
+};
+
+const populateHome = () => {
+  const homepage = document.createElement("div");
+  homepage.id = "homepage";
+  homepage.innerHTML = "LOS PRIMOS"
+  homepage.classList.add('homepage');
+  return homepage;
+};
+
+export { makeHomeTab, populateHome };
